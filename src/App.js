45a49a75faac9babd506ff1a052dbb6ec52be1c3
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-03-18 14:12:16 +0100

    started on home page component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import { Footer } from "./components/Footer";
 import { GlobalStyles } from "./components/styles/Global.styles";
 import { ThemeProvider } from "styled-components";
 import * as Styles from "./components/styles/index";
+import * as Page from "./components/pages/index";
 
 function App() {
   return (
     <ThemeProvider theme={Styles.theme}>
-      <>
-        <GlobalStyles />
-        <NavHeader />
-        <main className="test">hello world</main>
-        <Footer />
-      </>
+      <GlobalStyles />
+      <NavHeader />
+      <Page.Home />
+      <Footer />
     </ThemeProvider>
   );
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <h1>Home</h1>;
+import * as Styled from "../styles/index";
+
+export function Home() {
+  return (
+    <main>
+      <Styled.BaseContainer>
+        <h1>Home</h1>
+      </Styled.BaseContainer>
+    </main>
+  );
 }
