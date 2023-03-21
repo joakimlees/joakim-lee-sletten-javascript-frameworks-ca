a9f38f1e6d789af0d7aef062b237c84cdec131a8
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-03-22 00:41:19 +0100

    trying to make fetchAPI hook

--- src/api/getData.js
+++ src/api/getData.js
@@ -1,11 +1,37 @@
+import { useState, useEffect } from "react";
+/*
 export async function getData(url) {
+  const [test, setTest] = useState();
+
   try {
     const response = await fetch(url);
-    const result = await response.json();
-    return result;
+    const data = await response.json();
+    return data;
   } catch (error) {
     return error;
   } finally {
     console.log("loading complete");
   }
+}
+*/
+export function useFetch(url) {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    (async function () {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [url]);
+
+  return { data, error, loading };
 }
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,28 +1,33 @@
 import { API_ONLINE_SHOP_URL } from "../api/constants";
 import React, { useEffect, useState } from "react";
 import { ProductCard } from "./ProductCard";
+import { useFetch } from "../api/getData";
 
 export function Test() {
-  const [products, setProducts] = useState([]);
+  const { data } = useFetch(API_ONLINE_SHOP_URL);
 
-  // The useEffect will run once when the component first mounts
+  /*const [products, setProducts] = useState([]);
+
   useEffect(() => {
-    // Function that gets our posts
-    async function getData() {
-      const response = await fetch(API_ONLINE_SHOP_URL);
-      const json = await response.json();
-      // Setting our `posts` state to the API data we received
-      setProducts(json);
-    }
-    getData();
+    const getProducts = async () => {
+      setProducts(await getData(API_ONLINE_SHOP_URL));
+    };
+    getProducts();
   }, []);
 
-  return (
-    <article>
-      <h1>Products</h1>
-      {products.map(product => (
-        <ProductCard />
-      ))}
-    </article>
-  );
+  if (products instanceof Error) {
+    return <div>It was an error</div>;
+  }
+  */
+
+  if (data.length > 0) {
+    return (
+      <article>
+        <h1>products</h1>
+        {data.map(product => (
+          <div>{product.title}</div>
+        ))}
+      </article>
+    );
+  }
 }
