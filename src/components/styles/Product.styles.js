c82d1d31c219f110b6dbe4f37bae837e3c1d3366
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-03-23 19:51:40 +0100

    more styling product

--- src/components/styles/Product.styles.js
+++ src/components/styles/Product.styles.js
@@ -1,19 +1,32 @@
 import styled from "styled-components";
 
 export const Product = styled.main`
+  flex-grow: 1;
+
   h1 {
     color: red;
   }
 
   article {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
     border: solid;
   }
 
   .product-info-wrapper {
     display: flex;
     flex-wrap: wrap;
+
+    .image-wrapper {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 200px;
+
+      img {
+        width: 100%;
+        height: 100%;
+        min-width: 300px;
+        object-fit: cover;
+      }
+    }
   }
 `;
