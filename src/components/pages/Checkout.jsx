c8d4a8899e1c33ad69a5f212b074b96e2ee4928a
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-03-24 15:36:31 +0100

    checkout component started styling

--- src/components/pages/Checkout.jsx
+++ src/components/pages/Checkout.jsx
@@ -5,6 +5,22 @@ export function Checkout() {
     <Styled.Checkout>
       <Styled.BaseContainer>
         <h1>Checkout page</h1>
+        <article>
+          <h2>Your order</h2>
+          <article className="content-wrapper">
+            <div className="order-details-wrapper">
+              <div className="product-wrapper">
+                <h3>Products</h3>
+              </div>
+              <div className="summary-wrapper">
+                <h3>Summary</h3>
+              </div>
+            </div>
+            <div className="payment-info-wrapper">
+              <h3>payment information</h3>
+            </div>
+          </article>
+        </article>
       </Styled.BaseContainer>
     </Styled.Checkout>
   );
--- src/components/styles/Checkout.styles.js
+++ src/components/styles/Checkout.styles.js
@@ -1,3 +1,32 @@
 import styled from "styled-components";
 
-export const Checkout = styled.main``;
+export const Checkout = styled.main`
+  flex-grow: 1;
+  border: solid;
+  background-color: lightblue;
+
+  .content-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    .order-details-wrapper {
+      display: flex;
+      background-color: red;
+      gap: 20px;
+
+      .product-wrapper {
+        flex-grow: 1;
+        border: solid;
+      }
+
+      .summary-wrapper {
+        border: solid;
+      }
+    }
+
+    .payment-info-wrapper {
+      border: solid;
+    }
+  }
+`;
