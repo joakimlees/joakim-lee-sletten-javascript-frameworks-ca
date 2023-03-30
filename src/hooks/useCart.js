a747123bab374850e41e3737c4500e0db32b0053
joakimlees <91544170+joakimlees@users.noreply.github.com>
2023-03-30 18:24:15 +0200

    still struggling on local storage cart

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,21 +1,31 @@
 import { create } from "zustand";
+import useLocalStorage from "./useLocalStorage";
+import { useEffect } from "react";
 
 export const useCartStore = create(set => ({
   cart: [],
-  addProducts: product => set(state => ({ products: [{ id: product.id, title: product.title, price: product.price, imageUrl: product.imageUrl }, ...state.products] })),
-  clearProducts: () => set({ products: [] }),
+  setCart: items => {
+    set(state => ({ cart: localStorage.getItem("cart") }));
+  },
   useAddToCart: item => {
     set(state => ({ cart: [{ id: item.id, title: item.title, price: item.price, imageUrl: item.imageUrl }, ...state.cart] }));
+
+    localStorage.setItem("cart", JSON.stringify({ products: [{ ...item }] }));
   },
+  clearProducts: () => set({ cart: [] }),
 }));
 
 export function useCart() {
   const addProduct = useCartStore(state => state.useAddToCart);
   const clearProduct = useCartStore(state => state.clearProducts);
   const cart = useCartStore(state => state.cart);
+  const localCart = useCartStore(state => state.setCart);
+  const [valueInLocalStorage, setValueInLocalStorage] = useLocalStorage();
 
   function addToCart(id) {
     addProduct(id);
+    localCart(valueInLocalStorage);
+    //setValueInLocalStorage(cart);
 
     console.log(cart);
   }
